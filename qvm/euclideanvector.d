5ad0212a7cded55836b3ed0fa7cc9a3c3b96e6e8
root <kohei-coco@jcom.home.ne.jp>
2021-01-19 22:06:47 +0900

    vectorbaseのwarningを修正試み

--- qvm/euclideanvector.d
+++ qvm/euclideanvector.d
@@ -13,264 +13,264 @@ import sekitk.qvm.common: TypeOfSize, MatrixType, MajorOrder;
 
 mixin template VectorImpl(T, real Threshold)
 if(isFloatingPoint!T || isComplex!T){
-	import sekitk.qvm.common;
-	import sekitk.qvm.impl.vectorbase;
-	/************************************************************
-	 * Template of Eucledean vector
-	 ************************************************************/
-	struct Vector(TypeOfSize Size) if(Size > 0u){
-		mixin(VECTOR_BASE_IMPL);
+  import sekitk.qvm.common;
+  import sekitk.qvm.impl.vectorbase;
+  /************************************************************
+   * Template of Eucledean vector
+   ************************************************************/
+  struct Vector(TypeOfSize Size) if(Size > 0u){
+    mixin(VECTOR_BASE_IMPL);
 
-		// Constructors
-		@safe pure{
-			/// Initialize by a column matrix
-			this(MajorOrder MatOdr)(in Matrix!(Size, 1u,
-																				 MatrixType.dense,
-																				 MatOdr) vec) @nogc nothrow{
-				this._values[]= vec.v[];
-			}
-/+
-			this(T[] args ...)
-		  in(args.length == Size){
-				import std.algorithm: fill;
-				this._values[].fill(args);
-			}
-+/
-		}
+    // Constructors
+    @safe pure{
+      /// Initialize by a column matrix
+      this(MajorOrder MatOdr)(in Matrix!(Size, 1u,
+					 MatrixType.dense,
+					 MatOdr) vec) @nogc nothrow{
+	this._values[]= vec.v[];
+      }
+      /+
+       this(T[] args ...)
+       in(args.length == Size){
+       import std.algorithm: fill;
+       this._values[].fill(args);
+       }
+       +/
+    }
 
-		// Operators
-	  @safe pure nothrow @nogc const{
-			/// dot product
-			auto opBinary(string Op: "*")(in typeof(this) rhs){
-				return this.dotProdImpl(rhs);
-			}
+    // Operators
+    @safe pure nothrow @nogc const{
+      /// dot product
+      auto opBinary(string Op: "*")(in typeof(this) rhs){
+	return this.dotProdImpl(rhs);
+      }
 
-			/// cross product
-			static if(Size == 1u || Size == 3u || Size == 7u){
-				TypeOfThis opBinary(string Op: "%")(in TypeOfThis rhs){
-					T[Size] elm= void;
-					static if(Size == 1u){
-						elm[0]= T(0.0L);
-					}
-					else static if(Size == 3u){
-						elm[0]= this._values[1]*rhs._values[2]
-							-this._values[2]*rhs._values[1];
-						elm[1]= this._values[2]*rhs._values[0]
-							-this._values[0]*rhs._values[2];
-						elm[2]= this._values[0]*rhs._values[1]
-							-this._values[1]*rhs._values[0];
-					}
-					else{
-						elm[0]= this._values[1]*rhs._values[2]
-							-this._values[2]*rhs._values[1]
-							-this._values[3]*rhs._values[4]
-							+this._values[4]*rhs._values[3]
-							-this._values[5]*rhs._values[6]
-							+this._values[6]*rhs._values[5];
-						elm[1]= -this._values[0]*rhs._values[2]
-							+this._values[2]*rhs._values[0]
-							-this._values[3]*rhs._values[5]
-							+this._values[4]*rhs._values[6]
-							+this._values[5]*rhs._values[3]
-							-this._values[6]*rhs._values[4];
-						elm[2]= this._values[0]*rhs._values[1]
-							-this._values[1]*rhs._values[0]
-							-this._values[3]*rhs._values[6]
-							-this._values[4]*rhs._values[5]
-							+this._values[5]*rhs._values[4]
-							+this._values[6]*rhs._values[3];
-						elm[3]= this._values[0]*rhs._values[4]
-							+this._values[1]*rhs._values[5]
-							+this._values[2]*rhs._values[6]
-							-this._values[4]*rhs._values[0]
-							-this._values[5]*rhs._values[1]
-							-this._values[6]*rhs._values[2];
-						elm[4]= -this._values[0]*rhs._values[3]
-							-this._values[1]*rhs._values[6]
-							+this._values[2]*rhs._values[5]
-							+this._values[3]*rhs._values[0]
-							-this._values[5]*rhs._values[2]
-							+this._values[6]*rhs._values[1];
-						elm[5]= this._values[0]*rhs._values[6]
-							-this._values[1]*rhs._values[3]
-							-this._values[2]*rhs._values[4]
-							+this._values[3]*rhs._values[1]
-							+this._values[4]*rhs._values[2]
-							-this._values[6]*rhs._values[0];
-						elm[6]= -this._values[0]*rhs._values[5]
-							+this._values[1]*rhs._values[4]
-							-this._values[2]*rhs._values[3]
-							+this._values[3]*rhs._values[2]
-							-this._values[4]*rhs._values[1]
-							+this._values[5]*rhs._values[0];
-					}
-					return typeof(return)(elm);
-				}
-			}
-			else{
-				@disable auto opBinary(string OP: "^")(in Vector!Size rhs){}
-			}
-
-			// Comparison operator
-			/****************************************
-			 * Index operator
-			 *
-			 * Params:
-			 * 	idx= index, (1 ≤ idx ≤ SIZE)
-			 *
-			 * Returns:
-			 * 	element of "T[SIZE] _values" at (idx-1).
-			 *
-			 * Throws:
-			 *  RangeError
-			 ****************************************/
-			T opIndex(IdxType)(in IdxType idx)
-			if(isIntegral!IdxType)
-			in(idx > 0u)
-			in(idx <= Size){
-			  return _values[idx-1u];
-		  }
-
-			/****************************************
-			 * Slice operator
-			 *
-			 ****************************************/
-			T[] opSlice(IdxType)(in IdxType st, in IdxType en)
-			if(isIntegral!IdxType)
-		  in(st > 0u)
-			in(en <= Size)
-		  in(en > st){
-				return this._values[st-1u..en-1u].dup;
-		  }
-
-			/// length of the array
-			alias opDollar= Size;
-
-			/****************************************
-			 * Cast operator
-			 *
-			 * Returns:
-			 *  a column matrix
-			 ****************************************/
-		  Typ opCast(Typ: Matrix!(Size, 1u, MatrixType.dense, MatOdr), MatOdr)(){
-				return Typ(this._values);
-			}
-
-			Typ opCast(Typ: T[Size])(){
-				import std.array: staticArray;
-				return this._values[].staticArray!Size;
-			}
-		}
-
-	  // Reserved methods
-		@safe const{
-			/// convert to string
-			void toString(Writer, Char)(scope Writer wrt, scope const ref FormatSpec!Char formatSpec)
-		  if(isOutputRange!(Writer, const(Char)[])){
-				import std.format: formatValue;
-				import std.range.primitives: put;
-
-				wrt.put(PAREN_START);
-				foreach(TypeOfSize i; 0u..Size){
-					wrt.formatValue(this._values[i], formatSpec);
-					if(i < Size-1) wrt.put(DELIM);
-				}
-				wrt.put(PAREN_END);
-			}
-
-			/// ditto
-		  string toString(){
-				import std.exception: assumeUnique;
-
-				char[] buf;
-				{
-					enum size_t RESERVE_SIZE= PAREN_START.length	// Vector[
-						+"-123.45678".length*Size
-						+DELIM.length*(Size-1)
-						+PAREN_END.length;	// ]^T
-					buf.reserve(RESERVE_SIZE);
-				}
-
-				auto fmt= FormatSpec!char("%s");
-				toString((const(char)[] s){buf ~= s;}, fmt);
-				return (char[] bufMutable) @trusted{return assumeUnique(bufMutable);}(buf);
-			}
-			unittest{
-				double[4] temp= [1.2, -3.4, 5.6, -7.8];
-				auto vec= SekiTK!double.Vector!4u(temp);
-				assert(vec.toString() == "Vector[1.2, -3.4, 5.6, -7.8]^T");
-
-				import std.format: format;
-				assert(format("%.2f", vec) == "Vector[1.20, -3.40, 5.60, -7.80]^T");
-				assert(format("%4.1f", vec) == "Vector[ 1.2, -3.4,  5.6, -7.8]^T");
-			}
-		}
-
-	  // Other methods
-		@safe pure nothrow const{
-			Matrix!(1u, Size,
-							MatrixType.dense,
-							MatOdr) rowVector(MajorOrder MatOdr= MajorOrder.row)(){
-				return new typeof(return)(this._values[]);
-			}
-
-			static if(Size == 2u || Size == 3u){
-				T x() @nogc{return _values[0];}
-				T y() @nogc{return _values[1];}
-			}
-			static if(Size == 3u){
-				T z() @nogc{return _values[2];}
-			}
-
-			/**************************************
-			 * Outor product
-			 *
-			 * a.opProdOuter(b)= [[a[0]*b[0], a[0]*b[1], a[0]*b[2]],
-			 *                    [a[1]*b[0], a[1]*b[1], a[1]*b[2]],
-			 *                    [a[2]*b[0], a[2]*b[1], a[2]*b[2]]];
-			 ****************************************/
-			version(future){
-				Matrix!(Size, Size, MatrixType.dense, MajorOrder.row) opProdOuter(in TypeOfThis rhs){
-					T[arrayLength!(Size, Size, MatrixType.dense)] num= void;
-
-					foreach(i; 0u..Size)
-						foreach(j; 0u..Size){{num[i*Size+j]= this._values[i]*rhs._values[j];}}
-
-					return new typeof(return)(num);
-				}
-			}
-
-			/************************
-			 * Geometric product
-			 *
-			 * a.opProdGeometric(b)= ;
-			 ************************/
-			//opProdGeometric(){}
-
-			/+
-			static if(Size == 3u){
-				/**
-				 * a.opBinary!"%"(b)= a.crossProdOp * b
-				 **/
-				Matrix!(3, 3, MatrixType.SkewHermitian) crossProdOp() nothrow{
-					T[arrayLength!(3, 3, MatrixType.SkewHermitian)] num= [-a[2], a[1], -a[0]];
-					return new typeof(return)(num);
-				}
-
-				/**
-			 	 * b.opBinary!"%"(a)= a.crossProdOpRight * b
-				 */
-				Matrix!(3, 3, MatrixType.SkewHermitian) crossProdOpRight() nothrow{
-					T[arrayLength!(3, 3, MatrixType.SkewHermitian)] num= [a[2], -a[1], a[0]];
-					return new typeof(return)(num);
-				}
-			}
-+/
-		}
-
-	private:
-		enum string PAREN_START= "Vector[";
-		enum string PAREN_END= "]^T";
-		enum string DELIM= ", ";
+      /// cross product
+      static if(Size == 1u || Size == 3u || Size == 7u){
+	TypeOfThis opBinary(string Op: "%")(in TypeOfThis rhs){
+	  T[Size] elm= void;
+	  static if(Size == 1u){
+	    elm[0]= T(0.0L);
+	  }
+	  else static if(Size == 3u){
+	    elm[0]= this._values[1]*rhs._values[2]
+	      -this._values[2]*rhs._values[1];
+	    elm[1]= this._values[2]*rhs._values[0]
+	      -this._values[0]*rhs._values[2];
+	    elm[2]= this._values[0]*rhs._values[1]
+	      -this._values[1]*rhs._values[0];
+	  }
+	  else{
+	    elm[0]= this._values[1]*rhs._values[2]
+	      -this._values[2]*rhs._values[1]
+	      -this._values[3]*rhs._values[4]
+	      +this._values[4]*rhs._values[3]
+	      -this._values[5]*rhs._values[6]
+	      +this._values[6]*rhs._values[5];
+	    elm[1]= -this._values[0]*rhs._values[2]
+	      +this._values[2]*rhs._values[0]
+	      -this._values[3]*rhs._values[5]
+	      +this._values[4]*rhs._values[6]
+	      +this._values[5]*rhs._values[3]
+	      -this._values[6]*rhs._values[4];
+	    elm[2]= this._values[0]*rhs._values[1]
+	      -this._values[1]*rhs._values[0]
+	      -this._values[3]*rhs._values[6]
+	      -this._values[4]*rhs._values[5]
+	      +this._values[5]*rhs._values[4]
+	      +this._values[6]*rhs._values[3];
+	    elm[3]= this._values[0]*rhs._values[4]
+	      +this._values[1]*rhs._values[5]
+	      +this._values[2]*rhs._values[6]
+	      -this._values[4]*rhs._values[0]
+	      -this._values[5]*rhs._values[1]
+	      -this._values[6]*rhs._values[2];
+	    elm[4]= -this._values[0]*rhs._values[3]
+	      -this._values[1]*rhs._values[6]
+	      +this._values[2]*rhs._values[5]
+	      +this._values[3]*rhs._values[0]
+	      -this._values[5]*rhs._values[2]
+	      +this._values[6]*rhs._values[1];
+	    elm[5]= this._values[0]*rhs._values[6]
+	      -this._values[1]*rhs._values[3]
+	      -this._values[2]*rhs._values[4]
+	      +this._values[3]*rhs._values[1]
+	      +this._values[4]*rhs._values[2]
+	      -this._values[6]*rhs._values[0];
+	    elm[6]= -this._values[0]*rhs._values[5]
+	      +this._values[1]*rhs._values[4]
+	      -this._values[2]*rhs._values[3]
+	      +this._values[3]*rhs._values[2]
+	      -this._values[4]*rhs._values[1]
+	      +this._values[5]*rhs._values[0];
+	  }
+	  return typeof(return)(elm);
 	}
+      }
+      else{
+	@disable auto opBinary(string OP: "^")(in Vector!Size rhs){}
+      }
+
+      // Comparison operator
+      /****************************************
+       * Index operator
+       *
+       * Params:
+       * 	idx= index, (1 ≤ idx ≤ SIZE)
+       *
+       * Returns:
+       * 	element of "T[SIZE] _values" at (idx-1).
+       *
+       * Throws:
+       *  RangeError
+       ****************************************/
+      T opIndex(IdxType)(in IdxType idx)
+      if(isIntegral!IdxType)
+      in(idx > 0u)
+      in(idx <= Size){
+	return _values[idx-1u];
+      }
+
+      /****************************************
+       * Slice operator
+       *
+       ****************************************/
+      T[] opSlice(IdxType)(in IdxType st, in IdxType en)
+      if(isIntegral!IdxType)
+      in(st > 0u)
+      in(en <= Size)
+      in(en > st){
+	return this._values[st-1u..en-1u].dup;
+      }
+
+      /// length of the array
+      alias opDollar= Size;
+
+      /****************************************
+       * Cast operator
+       *
+       * Returns:
+       *  a column matrix
+       ****************************************/
+      Typ opCast(Typ: Matrix!(Size, 1u, MatrixType.dense, MatOdr), MatOdr)(){
+	return Typ(this._values);
+      }
+
+      Typ opCast(Typ: T[Size])(){
+	import std.array: staticArray;
+	return this._values[].staticArray!Size;
+      }
+    }
+
+    // Reserved methods
+    @safe const{
+      /// convert to string
+      void toString(Writer, Char)(scope Writer wrt, scope const ref FormatSpec!Char formatSpec)
+      if(isOutputRange!(Writer, const(Char)[])){
+	import std.format: formatValue;
+	import std.range.primitives: put;
+
+	wrt.put(PAREN_START);
+	foreach(TypeOfSize i; 0u..Size){
+	  wrt.formatValue(this._values[i], formatSpec);
+	  if(i < Size-1) wrt.put(DELIM);
+	}
+	wrt.put(PAREN_END);
+      }
+
+      /// ditto
+      string toString(){
+	import std.exception: assumeUnique;
+
+	char[] buf;
+	{
+	  enum size_t RESERVE_SIZE= PAREN_START.length	// Vector[
+	    +"-123.45678".length*Size
+	    +DELIM.length*(Size-1)
+	    +PAREN_END.length;	// ]^T
+	  buf.reserve(RESERVE_SIZE);
+	}
+
+	auto fmt= FormatSpec!char("%s");
+	toString((const(char)[] s){buf ~= s;}, fmt);
+	return (char[] bufMutable) @trusted{return assumeUnique(bufMutable);}(buf);
+      }
+      unittest{
+	double[4] temp= [1.2, -3.4, 5.6, -7.8];
+	auto vec= SekiTK!double.Vector!4u(temp);
+	assert(vec.toString() == "Vector[1.2, -3.4, 5.6, -7.8]^T");
+
+	import std.format: format;
+	assert(format("%.2f", vec) == "Vector[1.20, -3.40, 5.60, -7.80]^T");
+	assert(format("%4.1f", vec) == "Vector[ 1.2, -3.4,  5.6, -7.8]^T");
+      }
+    }
+
+    // Other methods
+    @safe pure nothrow const{
+      Matrix!(1u, Size,
+	      MatrixType.dense,
+	      MatOdr) rowVector(MajorOrder MatOdr= MajorOrder.row)(){
+	return new typeof(return)(this._values[]);
+      }
+
+      static if(Size == 2u || Size == 3u){
+	T x() @nogc{return _values[0];}
+	T y() @nogc{return _values[1];}
+      }
+      static if(Size == 3u){
+	T z() @nogc{return _values[2];}
+      }
+
+      /**************************************
+       * Outor product
+       *
+       * a.opProdOuter(b)= [[a[0]*b[0], a[0]*b[1], a[0]*b[2]],
+       *                    [a[1]*b[0], a[1]*b[1], a[1]*b[2]],
+       *                    [a[2]*b[0], a[2]*b[1], a[2]*b[2]]];
+       ****************************************/
+      version(future){
+	Matrix!(Size, Size, MatrixType.dense, MajorOrder.row) opProdOuter(in TypeOfThis rhs){
+	  T[arrayLength!(Size, Size, MatrixType.dense)] num= void;
+
+	  foreach(i; 0u..Size)
+	    foreach(j; 0u..Size){{num[i*Size+j]= this._values[i]*rhs._values[j];}}
+
+	  return new typeof(return)(num);
+	}
+      }
+
+      /************************
+       * Geometric product
+       *
+       * a.opProdGeometric(b)= ;
+       ************************/
+      //opProdGeometric(){}
+
+      /+
+       static if(Size == 3u){
+       /**
+       * a.opBinary!"%"(b)= a.crossProdOp * b
+       **/
+       Matrix!(3, 3, MatrixType.SkewHermitian) crossProdOp() nothrow{
+       T[arrayLength!(3, 3, MatrixType.SkewHermitian)] num= [-a[2], a[1], -a[0]];
+       return new typeof(return)(num);
+       }
+
+       /**
+       * b.opBinary!"%"(a)= a.crossProdOpRight * b
+       */
+       Matrix!(3, 3, MatrixType.SkewHermitian) crossProdOpRight() nothrow{
+       T[arrayLength!(3, 3, MatrixType.SkewHermitian)] num= [a[2], -a[1], a[0]];
+       return new typeof(return)(num);
+       }
+       }
++/
+    }
+
+  private:
+    enum string PAREN_START= "Vector[";
+    enum string PAREN_END= "]^T";
+    enum string DELIM= ", ";
+  }
 }
--- qvm/impl/vectorbase.d
+++ qvm/impl/vectorbase.d
@@ -16,230 +16,228 @@ import sekitk.qvm.common: TypeOfSize;
  * Implementation like a base class
  **************************************************************/
 enum string VECTOR_BASE_IMPL= q{
-	import std.format: FormatSpec;
-	import std.range: isOutputRange;
-	import std.complex: Complex;
-	import std.math: sqrt, approxEqual;
-	import std.meta: templateAnd;
-	import std.traits: Unqual, isDynamicArray, isStaticArray, isIntegral, ParameterTypeTuple;
+  import std.format: FormatSpec;
+  import std.range: isOutputRange;
+  import std.complex: Complex;
+  import std.math: sqrt, approxEqual;
+  import std.meta: templateAnd;
+  import std.traits: Unqual, isDynamicArray, isStaticArray, isIntegral, ParameterTypeTuple;
 
-	import sekitk.qvm.common: isPlusOrMinusSign;
-	import sekitk.qvm.exception: ZeroNorm;
+  import sekitk.qvm.common: isPlusOrMinusSign;
+  import sekitk.qvm.exception: ZeroNorm;
 
-	import sekitk.complex.pseudo: BaseRealType;
+  import sekitk.complex.pseudo: BaseRealType;
 
 private:
-	alias TypeOfThis= typeof(this);
+  alias TypeOfThis= typeof(this);
 
 public:
-	static if(isComplex!T){
-		alias CT= T;
-		alias RT= BaseRealType!T;
-	}
-	else{
-		alias CT= Complex!T;
-		alias RT= T;
-	}
-
-	// Constructors
-	@safe pure nothrow @nogc{
-		/// copy constructor
-		this(ref return scope inout TypeOfThis other){}
-
-		/****************************************
-		 * Params:
-		 *	num= a number of type T
-		 ****************************************/
-		this(Scalar)(scope const Scalar num)
-		if(is(Scalar: T)){
-			this._values[]= num;
-		}
-
-		/****************************************
-		 * Params:
-		 *	num= an array
-		 ****************************************/
-		this(ArrayType)(scope const ArrayType nums)
-	  if((isDynamicArray!ArrayType && is(ArrayType: T[])) ||
-			 (isStaticArray!ArrayType && is(ArrayType: T[Size])))
-		in(nums.length == Size, "mismatch array length"){
-		  this._values[]= nums[];
-		}
+  static if(isComplex!T){
+    alias CT= T;
+    alias RT= BaseRealType!T;
+  }
+  else{
+    alias CT= Complex!T;
+    alias RT= T;
   }
 
-	// Operators & reserved methods
-	@safe pure nothrow @nogc{
-		///
-		void opOpAssign(string Op)(in TypeOfThis rhs)
-		if(isPlusOrMinusSign!Op){
-			mixin("this._values[] "~ Op ~"= rhs._values[];");
-		}
+  // Constructors
+  @safe pure nothrow @nogc{
+    /// copy constructor
+    this(ref return scope inout TypeOfThis other){}
 
-		///
-		void opOpAssign(string Op, TypeR)(in TypeR rhs)
-		if((Op == "*" || Op == "/") && is(TypeR: T)){
-			mixin("this._values[] "~ Op~ "= rhs;");
-		}
+    /****************************************
+     * Params:
+     *	num= a number of type T
+     ****************************************/
+    this(Scalar)(scope const Scalar num)
+    if(is(Scalar: T)){
+      this._values[]= num;
+    }
 
-		///
-		TypeOfThis opUnary(string Op)() const
-		if(isPlusOrMinusSign!Op){
-			typeof(return) result;
-			static if(Op =="+"){
-				result= TypeOfThis(this);
-			}
-			else{
-				T[Size] num= _values[];
-				foreach(ref elm; num) elm= -elm;
-				result= TypeOfThis(num);
-			}
-			return result;
-		}
-
-		/******************************************
-		 * Params:
-		 *	rhs= other
-		 *
-		 * Returns:
-		 *	add or sub
-		 ******************************************/
-		TypeOfThis opBinary(string Op, TypeR: TypeOfThis)(in TypeR rhs) const
-		if(isPlusOrMinusSign!Op){
-			auto result= TypeOfThis(this);
-			result.opOpAssign!Op(rhs);
-			return result;
-		}
-
-		/******************************************
-		 * Params:
-		 *	rhs= a scalar
-		 ******************************************/
-		TypeOfThis opBinary(string Op, TypeR)(in TypeR rhs) const
-		if((Op == "*" || Op == "/") && is(TypeR: T)){
-			auto result= TypeOfThis(this._values);
-			result.opOpAssign!Op(rhs);
-			return result;
-		}
-
-		/******************************************
-		 * Params:
-		 *	lhs= a scalar
-		 ******************************************/
-		TypeOfThis opBinaryRight(string Op: "*", TypeL)(in TypeL lhs) const
-		if(is(TypeL: T)){
-			auto result= TypeOfThis(this._values);
-			result.opBinary!Op(lhs);
-			return result;
-		}
-
-		/******************************************
-		 * equality
-		 *
-		 * Params:
-		 *	rhs= other
-		 ******************************************/
-		bool opEquals()(auto ref const TypeOfThis rhs) const{
-			foreach(idx; 0u..Size){
-				if(!approxEqual(this._values[idx], rhs._values[idx])) return false;
-				else continue;
-			}
-			return true;
-		}
+    /****************************************
+     * Params:
+     *	num= an array
+     ****************************************/
+    this(ArrayType)(scope const ArrayType nums)
+    if((isDynamicArray!ArrayType && is(ArrayType: T[])) ||
+       (isStaticArray!ArrayType && is(ArrayType: T[Size])))
+    in(nums.length == Size, "mismatch array length"){
+      this._values[]= nums[];
+    }
   }
 
-	@safe{
-		///
-		int opApply(Dg)(scope Dg dg)
-		if(ParameterTypeTuple!Dg.length == 1){
-			typeof(return) result= 0;
-			foreach(elm; _values) result= dg(elm);
+  // Operators & reserved methods
+  @safe pure nothrow @nogc{
+    ///
+    void opOpAssign(string Op)(in TypeOfThis rhs)
+    if(isPlusOrMinusSign!Op){
+      mixin("this._values[] "~ Op ~"= rhs._values[];");
+    }
 
-			return result;
-		}
+    ///
+    void opOpAssign(string Op, TypeR)(in TypeR rhs)
+    if((Op == "*" || Op == "/") && is(TypeR: T)){
+      mixin("this._values[] "~ Op~ "= rhs;");
+    }
 
-		///
-		int opApplyReverse(Dg)(scope Dg dg) if(ParameterTypeTuple!Dg.length == 1){
-			typeof(return) result= 0;
-			foreach_reverse(elm; _values) result= dg(elm);
+    ///
+    TypeOfThis opUnary(string Op)() const
+    if(isPlusOrMinusSign!Op){
+      typeof(return) result;
+      static if(Op =="+"){
+	result= TypeOfThis(this);
+      }
+      else{
+	T[Size] num= _values[];
+	foreach(ref elm; num) elm= -elm;
+	result= TypeOfThis(num);
+      }
+      return result;
+    }
 
-			return result;
-		}
-	}
+    /******************************************
+     * Params:
+     *	rhs= other
+     *
+     * Returns:
+     *	add or sub
+     ******************************************/
+    TypeOfThis opBinary(string Op, TypeR: TypeOfThis)(in TypeR rhs) const
+    if(isPlusOrMinusSign!Op){
+      auto result= TypeOfThis(this);
+      result.opOpAssign!(Op, TypeR)(rhs);
+      return result;
+    }
 
-	// Other methods
+    /******************************************
+     * Params:
+     *	rhs= a scalar
+     ******************************************/
+    TypeOfThis opBinary(string Op, TypeR)(in TypeR rhs) const
+    if((Op == "*" || Op == "/") && is(TypeR: T)){
+      auto result= TypeOfThis(this._values);
+      result.opOpAssign!(Op, TypeR)(rhs);
+      return result;
+    }
+
+    /******************************************
+     * Params:
+     *	lhs= a scalar
+     ******************************************/
+    TypeOfThis opBinaryRight(string Op: "*", TypeL)(in TypeL lhs) const
+    if(is(TypeL: T)){
+      auto result= TypeOfThis(this._values);
+      result.opBinary!Op(lhs);
+      return result;
+    }
+
+    /******************************************
+     * equality
+     *
+     * Params:
+     *	rhs= other
+     ******************************************/
+    bool opEquals()(auto ref const TypeOfThis rhs) const{
+      foreach(idx; 0u..Size){
+	if(!approxEqual(this._values[idx], rhs._values[idx])) return false;
+	else continue;
+      }
+      return true;
+    }
+  }
+
+  @safe{
+    ///
+    int opApply(Dg)(scope Dg dg)
+      if(ParameterTypeTuple!Dg.length == 1){
+	typeof(return) result= 0;
+	foreach(elm; _values) result= dg(elm);
+	return result;
+      }
+
+    ///
+    int opApplyReverse(Dg)(scope Dg dg) if(ParameterTypeTuple!Dg.length == 1){
+      typeof(return) result= 0;
+      foreach_reverse(elm; _values) result= dg(elm);
+      return result;
+    }
+  }
+
+  // Other methods
   @safe pure const{
-		/****************************************
-		 * Returns:
-		 *	Euclidean norm
-		 ****************************************/
-		RT norm() nothrow @nogc @property{
-			return sqrt(this.dotProdImpl(this));
-		}
+    /****************************************
+     * Returns:
+     *	Euclidean norm
+     ****************************************/
+    RT norm() nothrow @nogc @property{
+      return sqrt(this.dotProdImpl(this));
+    }
 
-		/******************************************
-		 * Returns:
-		 *	unit vector
-		 *
-		 * Throws:
-		 *	ZeroNorm
-		 ******************************************/
-		TypeOfThis unit() @property{
-			T vecLen= this.norm;
-			typeof(return) result= TypeOfThis(this);
+    /******************************************
+     * Returns:
+     *	unit vector
+     *
+     * Throws:
+     *	ZeroNorm
+     ******************************************/
+    TypeOfThis unit() @property{
+      T vecLen= this.norm;
+      typeof(return) result= TypeOfThis(this);
 
-			if(!approxEqualZero(vecLen)){
-				result /= vecLen;
-			}
-			else{
-				throw new ZeroNorm;
-			}
+      if(!approxEqualZero(vecLen)){
+	result /= vecLen;
+      }
+      else{
+	throw new ZeroNorm;
+      }
 
-			return result;
-		}
+      return result;
+    }
 
-		/******************************************
-		 * Params:
-		 *	rhs= other
-		 ******************************************/
-	  bool isApproxEqualTo(in TypeOfThis rhs) nothrow @nogc{
-			typeof(return) result= true;
-			foreach(i; 0u..Size){
-				if( !this.approxEqualZero(this._values[i]-rhs._values[i]) ){
-					result= false;
-					break;
-				}
-			}
-			return result;
-		}
+    /******************************************
+     * Params:
+     *	rhs= other
+     ******************************************/
+    bool isApproxEqualTo(in TypeOfThis rhs) nothrow @nogc{
+      typeof(return) result= true;
+      foreach(i; 0u..Size){
+	if( !this.approxEqualZero(this._values[i]-rhs._values[i]) ){
+	  result= false;
+	  break;
+	}
+      }
+      return result;
+    }
 
-	  /****************************************
-		 * Returns:
-		 *	true
-		 ******************************************/
-	  bool isNormalized() nothrow @nogc @property{
-			return approxEqual(this.norm, RT(1.0L));
-		}
+    /****************************************
+     * Returns:
+     *	true
+     ******************************************/
+    bool isNormalized() nothrow @nogc @property{
+      return approxEqual(this.norm, RT(1.0L));
+    }
   }
 
 private:
-	@safe pure nothrow @nogc const{
-		// alias function this.approxEqualZero(num)= sekitk.approx.approxEqualZero!(T, Threshold)(num);
-		bool approxEqualZero(in T num){
-			import sekitk.approx: approxEqualZero;
-			return approxEqualZero!(T, Threshold)(num);
-		}
+  @safe pure nothrow @nogc const{
+    // alias function this.approxEqualZero(num)= sekitk.approx.approxEqualZero!(T, Threshold)(num);
+    bool approxEqualZero(in T num){
+      import sekitk.approx: approxEqualZero;
+      return approxEqualZero!(T, Threshold)(num);
+    }
 
-		/******************************************
-		 * Dot product
-		 ******************************************/
-		RT dotProdImpl(in TypeOfThis rhs){
-			import sekitk.complex.pseudo: conj;
-			RT sum= RT(0.0L);
-			foreach(i; 0u..Size) sum += this._values[i]*rhs._values[i].conj;
-			return sum;
-		}
-	}
+    /******************************************
+     * Dot product
+     ******************************************/
+    RT dotProdImpl(in TypeOfThis rhs){
+      import sekitk.complex.pseudo: conj;
+      RT sum= RT(0.0L);
+      foreach(i; 0u..Size) sum += this._values[i]*rhs._values[i].conj;
+      return sum;
+    }
+  }
 
 package:
-	T[Size] _values;
+  T[Size] _values;
 };
